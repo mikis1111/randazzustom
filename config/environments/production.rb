32f6707067e3b1c63515507a1a853328114de790
ms <michelesaladino@micheles-MacBook-Pro-3.local>
2024-02-26 17:47:11 +0100

    production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.assets.precompile = ['*.js', '*.css', '**/*.js', '**/*.css', '*.jpg', '*.png', '*.gif', '*.ico']
   config.assets.digest = true
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
