281b887d9cf76a92104248e2451ad64b7f890940
ms <michelesaladino@micheles-MacBook-Pro-3.local>
2024-03-05 08:25:58 +0100

    changed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,4 @@
-const path = require('path');
-
 module.exports = {
-  mode: 'development', // o 'production'
-  entry: './src/index.js', // Punto di ingresso dell'applicazione
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  devtool: 'source-map', // Imposta qui l'opzione devtool
+  entry: './src/index.js',
+  // altre configurazioni...
 };
