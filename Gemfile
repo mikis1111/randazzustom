faa3c60d570e70d126081fa972367f59db2246c2
ms <michelesaladino@micheles-MBP-3.Home>
2024-03-08 15:46:03 +0100

    upgrade rails from 6 to 7 version

--- Gemfile
+++ Gemfile
@@ -5,10 +5,12 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '3.0.0'
 gem 'bundler', '2.5.6'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
-gem 'rails', '~> 6.1.6'
+gem "rails", "~> 7.0.0"
 # Use sqlite3 as the database for Active Record
 
 gem 'pg', '~> 1.2'
+gem "sprockets-rails"
+gem 'rack-timeout'
 
 # Use Puma as the app server
 gem 'puma', '~> 5.0'
--- config/application.rb
+++ config/application.rb
@@ -9,9 +9,8 @@ Bundler.require(*Rails.groups)
 module Tomweb
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
-    config.assets.initialize_on_precompile = false
-    config.assets.css_compressor = nil
+    config.load_defaults 7.0
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
